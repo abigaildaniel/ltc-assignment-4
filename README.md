f9d407aee987bfd31ed4ef64e55a97ab8f32ad96
Elias Jackson <ejthedj0@gmail.com>
2023-04-18 11:31:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# OpenSesame Catalog Analysis
+
+This is a Node.js script that tells you how many hours (or days) it would take to complete every course in the OpenSesame Catalog!
+
+## Instructions
+
+### Editing the Code
+
+1. Open the [index.js](index.js) file and copy all of the code into the [Online Node.js Compiler](https://www.tutorialspoint.com/execute_nodejs_online.php).
+2. Modify the code so that it contains accurate numbers for the [OpenSesame Catalog](https://www.opensesame.com/online-training-courses/search) and calculates the number of hours that it would take to complete every single course.
+3. To test your code, click the "Execute" button in the top left.
+
+### Submitting the Code
+
+Once your code is ready, you'll want to create your own GitHub repository for it.
+
+1. Make sure you are logged into your GitHub account.
+2. Create a [fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) (basically a clone) of this repository by following the "Forking a repository" steps in [this guide](https://docs.github.com/en/get-started/quickstart/fork-a-repo?tool=webui#forking-a-repository).
+3. On your forked repository, open the index.js file and click the "edit" icon (looks like a pencil).
+4. Replace this with your working code from the Online Node.js Compiler.
+5. Send me the URL for your repository in Slack so I can verify it! :tada:
---
+++ index.js
@@ -0,0 +1,26 @@
+const myName = 'insert name here';
+const mentorName = 'insert mentor here';
+
+const totalCourses = 0; // Total amount of courses in the OpenSesame catalog
+const courses10Min = 0; // Amount of courses 1-10 min (assume 10 min for calculation)
+const courses30Min = 0; // Amount of courses 11-30 min (assume 30 min for calculation)
+const courses59Min = 0; // Amount of courses 31-59 min (assume 59 min for calculation)
+const courses1HourPlus = 0; // Amount of courses 1 hour+ (assume 3 hours for calculation)
+
+// Feel free to add code here for the calculation
+
+const totalHoursOfAllCourses = 0; // Amount of time to view all courses (measured in HOURS)
+
+// Only change code ABOVE this line!
+
+const totalDaysOfAllCourses = totalHoursOfAllCourses / 24;
+console.log(`\nName: ${myName}`);
+console.log(`Mentor name: ${mentorName}`);
+
+console.log(`\n=====COURSE CATALOG ANALYSIS=====\n`);
+console.log(`There are ${totalCourses} total courses in the OpenSesame catalog.`);
+console.log(`${courses10Min} are 1-10 min long. For calculation, we'll assume that they're all 10 min.`)
+console.log(`${courses30Min} are 11-30 min long. For calculation, we'll assume that they're all 30 min.`)
+console.log(`${courses59Min} are 31-59 min long. For calculation, we'll assume that they're all 59 min.`)
+console.log(`${courses1HourPlus} are longer than an hour. For calculation, we'll assume that they're all 3 hours.`)
+console.log(`Using these estimates, it would take about ${totalHoursOfAllCourses} hours to take all of the courses that OpenSesame offers! That's ${totalDaysOfAllCourses} days!`)
